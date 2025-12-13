bada644609cd9e6edaa9ba0b856a377910d00836
meghanam555 <meghana10000coders@gmail.com>
2025-12-13 15:54:14 +0530

    JSX Rule Code

---
+++ 13-12-2025/JSXConcept/src/App.jsx
@@ -0,0 +1,15 @@
+import Sample from "./Component/Sample"
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Sample />
+    </>
+  )
+}
+
+export default App
---
+++ 13-12-2025/JSXConcept/src/Component/Sample.jsx
@@ -0,0 +1,14 @@
+function Sample(){
+    return(
+        <div>
+            <h1>Today is Saturday, Party Mood</h1>
+            <h2>Enjoy</h2>
+            <div>
+            <h3>Tommorow is Holiday</h3>
+        </div>
+        </div>
+        
+    )
+    
+}
+export default Sample;
